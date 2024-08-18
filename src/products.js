ff717f303a565959b261ab7c160a81cca47bf1be
Abdulkadir <qansax30@hotmail.com>
2024-08-18 19:47:08 +0200

    BERSHKA

---
+++ src/products.js
@@ -0,0 +1,24 @@
+import product6 from "./assets/asset 6 straight jeans.jpg";
+import product7 from "./assets/asset 7 shoes1.jpg";
+import product8 from "./assets/asset 8 shoes2.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 6,
+    productName: "straight fit jeans",
+    price: 45.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Men’s multi-piece sneakers",
+    price: 69.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Men’s multi-piece sneakers",
+    price: 69.0,
+    productImage: product8,
+  },
+];
